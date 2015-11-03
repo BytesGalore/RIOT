3a2e76babda31e8d113ca76f73fec5d3664691e0
BytesGalore <Martin.Landsmann@HAW-Hamburg.de>
2015-11-03 21:16:02 +0100

    test

--- sys/net/gnrc/routing/dsr/basic_functions.c
+++ sys/net/gnrc/routing/dsr/basic_functions.c
@@ -63,9 +63,9 @@ static int _receive(gnrc_pktsnip_t* pkt)
     }
     
     udp_hdr_t* hdr = (udp_hdr_t*)snip->data;
-    printf("udp size: %d %d\n", byteorder_ntohs(hdr->length), sizeof(dsr_opt_hdr_t));
+    printf("udp size: %d %d\n", byteorder_ntohs(hdr->length), sizeof(udp_hdr_t));
     
-    dsr_opt_hdr_t* dsr_hdr = (dsr_opt_hdr_t*)(((uint8_t*)snip->data) + sizeof(dsr_opt_hdr_t));
+    dsr_opt_hdr_t* dsr_hdr = (dsr_opt_hdr_t*)(((uint8_t*)snip->data) + sizeof(udp_hdr_t));
     
     print_dsr_head(dsr_hdr);
     if( snip != NULL && snip->type == GNRC_NETTYPE_UDP) {
